fe86d33305eb156e737165de5b2919613f5377b9
thatbenlad <133782395+thatbenlad@users.noreply.github.com>
2026-09-11 09:58:48 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEXqFqb4_owEtXfPAgijDwz1K5eq1RRsQ",
+  authDomain: "run-club-d06b9.firebaseapp.com",
+  projectId: "run-club-d06b9",
+  storageBucket: "run-club-d06b9.firebasestorage.app",
+  messagingSenderId: "992630611751",
+  appId: "1:992630611751:web:eca0a8c5e85238a3a41302"
+};
